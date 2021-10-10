24264adf7926dca2fdf505e62abb99cce887ced7
TomaszAugustyn <lourian@poczta.fm>
2021-10-11 00:40:31 +0200

    Extend examples in demo

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ public:
 
 class B {
 public:
+    B() { std::cout << "constructing B ...\n"; };
     void foo() {
         std::cout << "non-static foo \n";
         std::vector<int> vec{ 1, 55, 78, 3, 11, 7, 90 };
@@ -19,23 +20,46 @@ public:
     }
 };
 
-int fibonacci(int n) {
+constexpr unsigned fibonacci(unsigned n) {
     if (n <= 1)
         return n;
     return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void print() {
+    std::cout << "empty function, called last.\n";
+}
+
+template <typename T, typename... Types>
+void print(T var1, Types... var2) {
+    std::cout << var1 << std::endl;
+    print(var2...);
+}
+
+inline int cube(int s) {
+    return s * s * s;
 }
 
 int main() {
     // Test logging static member methods.
     A::foo();
 
+    // Test logging lambdas
+    auto isLessThan = [](auto a, auto b) { return a < b; };
+    bool out = isLessThan(3, 3.14);
+    std::cout << "isLessThan: " << std::boolalpha << out << std::endl;
+
+    // Test logging user-defined default constructor
+    B b;
     // Test logging non-static member methods with calls to std
     // functions/containers (which should not be instrumented).
-    B b;
     b.foo();
+    // Test logging constexpr function
     fibonacci(6);
-    A::foo();
-    std::cout << "HELLO WORLD!" << std::endl;
+    // Test logging variadic function templates
+    print(44, 3.14159, "whatever\n");
+    // Test logging inline function
+    cube(3);
 
     return 0;
 }
